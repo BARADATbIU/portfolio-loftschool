bfb5e8faa0654a0f218f1b1c40bb3857c25ca7c7
Dmitry Kharlamov <newdimaak@gmail.com>
2018-06-10 09:30:48 +0300

    First commit

---
+++ src/assets/styles/variables.js
@@ -0,0 +1,10 @@
+module.exports = {
+  white: "#f4f5f0",
+  beigelight: "#efeee9",
+  beige: "#eeebdf",
+  green: "#009688",
+  greenLight: "#00bfa5",
+  grey: "#455a64",
+  greyLight: "#d6d6d6",
+  greyDark: "#373e42"
+};
