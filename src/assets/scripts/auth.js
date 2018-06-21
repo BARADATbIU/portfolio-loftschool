6c2e0a120d3a2289eb84639fec85ef12ae912430
Dmitry Kharlamov <newdimaak@gmail.com>
2018-06-21 13:53:41 +0300

    week 3.5 - disable parallax for mobile, corrections

--- src/assets/scripts/auth.js
+++ src/assets/scripts/auth.js
@@ -1,5 +1,7 @@
 import flip from "./modules/flip";
 import moveParallax from "./modules/moveParallax";
+import isMobileDevice from "./modules/isMobileDevice";
+
+if (!isMobileDevice()) moveParallax();
 
 flip();
-moveParallax();
